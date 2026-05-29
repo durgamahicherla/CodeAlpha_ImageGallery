eb74b9b708dafe5bc3fbe254e30db62b798d6768
Durga_Mahicherla <durgamahicherla@gmail.com>
2026-05-29 11:38:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function openLightbox(src){
+    document.getElementById("lightbox").style.display="flex";
+    document.getElementById("lightbox-img").src=src;
+}
+
+function closeLightbox(){
+    document.getElementById("lightbox").style.display="none";
+}
+
+function filterImages(category){
+
+    let images=document.querySelectorAll(".image");
+
+    images.forEach(image=>{
+
+        if(category==="all"){
+            image.style.display="block";
+        }
+        else{
+            if(image.classList.contains(category)){
+                image.style.display="block";
+            }
+            else{
+                image.style.display="none";
+            }
+        }
+
+    });
+
+}
